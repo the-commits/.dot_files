3532266ed4e18a3cde7ee4e9d40c81bf856c19ec
Magnus Åberg <himself@magnusaberg.me>
2023-05-20 14:18:23 +0200

    Add keymaps and plugins

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,34 @@
+-- Use space as leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- When text is wrapped move normally within it
+vim.keymap.set('n' , 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n' , 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Easy alias for typos
+vim.keymap.set('n', 'q:', ':q')
+
+-- Insert trailing ; or , from insert mode
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
+
+-- Quickly clear search highligting
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
+
+-- Open the current file in the default program (Mac should use 'open')
+vim.keymap.set('n', '<Leader>x', ':!xdg-open %<CR><CR>')
+
+-- Move lines up and down.
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
