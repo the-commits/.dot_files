dda9a005e09ff38ff3bf8131d59da262fa7db80d
MangeBang <himself@magnusaberg.me>
2023-08-28 09:53:42 +0200

    Remove polybar

--- polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Terminate already running bar instances
-killall -q polybar
-
-#launch polybar
-echo "---" | tee -a /tmp/toppolybar.log
-polybar example >> /tmp/toppolybar.log
