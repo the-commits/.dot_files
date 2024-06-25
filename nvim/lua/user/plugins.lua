3e5c4ff43e1aaa25677472196e95fbcd657b26bd
Magnus Åberg <himself@magnusaberg.me>
2024-06-25 09:48:11 +0200

    Add keymaps and plugins

---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,118 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').reset()
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+  --display = {
+  --  open_fn = function()
+  --    return require('packer.util').float({ border = 'solid' })
+  --  end,
+  --},
+})
+
+local use = require('packer').use
+
+-- Packer can manage itself.
+use('wbthomason/packer.nvim')
+
+-- Commenting support.
+use('tpope/vim-commentary')
+
+-- Add, change and delete surrounding text.
+use('tpope/vim-surround')
+
+-- Indent autodetection with editorconfig support.
+use('tpope/vim-sleuth')
+
+-- Allow plugins to enable repeating of commands
+use('tpope/vim-repeat')
+
+-- Navigate between vim and tmux panes
+use('christoomey/vim-tmux-navigator')
+
+-- Add more languages.
+use('sheerun/vim-polyglot')
+
+-- Jump to the last place
+use('farmergreg/vim-lastplace')
+
+-- Automatically create parent dir
+use('jessarcher/vim-heritage')
+
+-- Automatically set the working directory to the project root.
+use({
+  'airblade/vim-rooter',
+  setup = function()
+    -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+    vim.g.rooter_manual_only = 1
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
+})
+
+-- Automatically add closing brackets, quotes, etc.
+use({
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup()
+  end,
+})
+
+-- Add smooth scrolling to avoid jarring jumps
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('neoscroll').setup()
+  end,
+})
+
+-- All closing buffers without closing the split window.
+use({
+  'famiu/bufdelete.nvim',
+  config = function()
+    vim.keymap.set('n', '<Leader>q', ':bdelete<CR>')
+  end,
+})
+
+-- Split arrays and methods onto multiple lines, or join them back up.
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+    vim.g.splitjoin_trailing_comma = 1
+    vim.g.splitjoin_php_method_chain_full = 1
+  end,
+})
+
+-- Automatically fix indentation when pasting code.
+use({
+  'sickill/vim-pasta',
+  config = function()
+    vim.g.pasta_disabled_filetypes = { 'fugitive' }
+  end,
+})
+
+-- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
+if packer_bootstrap then
+    require('packer').sync()
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
