2a0b1be539e1eb64e8e932bc15710bd56377c131
Magnus Åberg <himself@magnusaberg.me>
2024-06-25 09:49:28 +0200

    Add find files with telescop and vim-tree

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -104,6 +104,27 @@ use({
   end,
 })
 
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'kyazdani42/nvim-web-devicons',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  },
+  config = function()
+    require('user/plugins/telescope')
+    end
+})
+
+use({
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('user/plugins/nvim-tree')
+    end,
+
+  })
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
 if packer_bootstrap then
