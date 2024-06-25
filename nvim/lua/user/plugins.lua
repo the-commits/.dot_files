17147a4f11bd1f5913ad7e2c6dc68bbe20b0d15b
Magnus Åberg <himself@magnusaberg.me>
2024-06-25 09:49:28 +0200

    Make floaterm split 33 percent of the screen bottom

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -217,8 +217,8 @@ use({
 use({
   'voldikss/vim-floaterm',
   config = function()
-    vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.8
+    vim.g.floaterm_height = 0.33
+    vim.g.floaterm_wintype = 'split'
     vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
     vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
     vim.cmd([[
