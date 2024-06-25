02c18d1f153b971e813e6a953fe98eb7f73dfe55
Magnus Åberg <himself@magnusaberg.me>
2024-06-25 09:49:28 +0200

    Add plugin configs

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,13 +1,18 @@
 require('nvim-tree').setup({
-    git = {
-      ignore = falsem
-    },
-    renderer = {
-      group_empty = true,
-      indent_markers = {
-        enable = true,
+  git = {
+    ignore = false,
+  },
+  renderer = {
+    group_empty = true,
+    icons = {
+      show = {
+        folder_arrow = false,
       },
     },
+    indent_markers = {
+      enable = true,
+    },
+  },
 })
 
 vim.keymap.set('n', '<Leader>n', ':NvimTreeFindFileToggle<CR>')
