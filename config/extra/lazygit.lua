f7b0f437cbbd52e3b4e8aa401eade39cad772b48
Magnus Åberg <himself@magnusaberg.me>
2024-06-25 09:50:36 +0200

    Ignore nerd fonts, update tmux and config and neovim namings

---
+++ config/extra/lazygit.lua
@@ -0,0 +1,10 @@
+-- nvim v0.8.0
+require("lazy").setup({
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+})
